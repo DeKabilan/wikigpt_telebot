745d28523a6f35172b256581433d65ecab9be24e
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2024-08-06 22:43:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ results=''
 links=[]
 
 
-bot = telebot.TeleBot("Your-Bot-Token-Goes-Here")
+bot = telebot.TeleBot("5983765325:AAHcQ33r6WuWrxYReZONwLkVQHZHOm2q4rw")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
